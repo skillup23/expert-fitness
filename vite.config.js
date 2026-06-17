614f4315d92b3fc4c4f0e78623fdac6e17c7b7ff
Robert Anisimov <skillup23@yandex.ru>
2026-06-17 15:13:29 +0300

    пререндеринг страниц для Seo в1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import prerender from '@prerenderer/rollup-plugin';
 
-// https://vitejs.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    prerender({
+      // Указываем плагину, где лежит скомпилированный проект
+      staticDir: path.resolve(__dirname, 'dist'),
+      // Список роутов, которые нужно отрендерить в статический HTML
+      routes: ['/', '/group', '/privacy'],
+      rendererOptions: {
+        // Указываем путь к стабильной версии установленного Google Chrome
+        executablePath:
+          'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+        // Отключаем песочницу (частая причина ошибки 3221225477 в Windows)
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
